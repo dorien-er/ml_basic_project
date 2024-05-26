6b62d8320537c13735f497f163993d5f1966ffd0
dorien-er <roosen.dorien@gmail.com>
2024-05-26 12:33:48 +0200

    project kickstarter

---
+++ build/lib/ml_code/mod.py
@@ -0,0 +1,8 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+def lin_reg(x, y):
+    x = np.array(x).reshape(-1, 1)
+    y = np.array(y).reshape(-1, 1)
+    model = LinearRegression().fit(x, y)
+    return model.coef_[0][0], model.intercept_[0]
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+
+requirements=[ x.strip() for x in content]
+
+setup(name="ml_code",
+      description="Machine Learning Code",
+      packages=["ml_code"],
+      install_requires=requirements)
+
+
+

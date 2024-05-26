8cf0586a42ace2eb4f1c56b418897b2bbe7cdae5
dorien-er <roosen.dorien@gmail.com>
2024-05-26 12:50:45 +0200

    wrong commit

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
+
 with open("requirements.txt") as f:
     content = f.readlines()
 
 
-requirements=[x.strip() for x in content]
+requirements = [x.strip() for x in content]
 
 setup(name="ml_code",
       description="Machine Learning Code",

0587128fd75752a1f9ce38d03c1c64eab4268bc0
dorien-er <roosen.dorien@gmail.com>
2024-05-26 12:54:00 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
-
 with open("requirements.txt") as f:
     content = f.readlines()
 
 
-requirements = [x.strip() for x in content]
+requirements=[x.strip() for x in content]
 
 setup(name="ml_code",
       description="Machine Learning Code",
